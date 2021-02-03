83b729a1377af1b361e05aee5831a711c2780cfe
fullname <fullname@pop-os.localdomain>
2021-02-02 20:44:43 -0500

    toto

---
+++ net/ssl/getips.sh
@@ -0,0 +1,51 @@
+
+resolvectl query www.google.com
+resolvectl query www.duckduckgo.com
+resolvectl query www.youtube.com
+resolvectl query www.yandex.com
+resolvectl query www.github.com
+resolvectl query fiximate.fixtrading.org
+resolvectl query www.amazon.com
+resolvectl query www.amazon.ca
+resolvectl query www.amazon.fr
+resolvectl query arxiv.org
+resolvectl query www.desjardins.com
+resolvectl query www.ca-cmds.fr
+resolvectl query www.labanquepostale.fr
+resolvectl query aws.amazon.com
+resolvectl query www.mailgun.com
+resolvectl query www.mailjet.com
+resolvectl query www.sendinblue.com
+resolvectl query sendgrid.com
+resolvectl query www.hckrnews.com
+resolvectl query www.zerohedge.com
+resolvectl query www.lemonde.fr
+resolvectl query www.lapresse.ca
+resolvectl query montrealgazette.com
+resolvectl query www.infosva.org
+resolvectl query www.svaplus.fr
+resolvectl query www.zdnet.com 
+resolvectl query www.wired.com
+resolvectl query www.cyberscoop.com
+resolvectl query www.enisa.europa.eu
+resolvectl query threatpost.com
+resolvectl query www.infosecurity-magazine.com 
+resolvectl query hackernoon.com 
+resolvectl query thefreshloaf.com 
+resolvectl query www.minoterie-bourseau.com
+resolvectl query www.tube8.com
+resolvectl query www.youporn.com
+resolvectl query www.porntube.com
+resolvectl query www.sudouest.fr 
+resolvectl query www.renodepot.com
+resolvectl query www.rona.com
+resolvectl query thehill.com 
+resolvectl query www.nytimes.com
+resolvectl query www.cnn.com 
+resolvectl query www.rt.com
+resolvectl query www.foxnews.com
+resolvectl query www.cnbc.com
+
+
+
+
